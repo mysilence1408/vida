1db42f4c70819049fc06d36990f880b87a9ac08d
mysilence1408 <mysilence1408@gmail.com>
2025-10-25 23:58:04 +0300

    import removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Features from "@/components/Home/Features";
 import Hero from "@/components/Home/Hero";
 import Benefits from "@/components/Home/Benifits";
 import Video from "@/components/Home/Video";
-import Whoarewe from "@/components/Home/Whoarewe";
 import WhyChoose from "@/components/Home/WhyChoose";
 import WhoCanUse from "@/components/Home/WhoCanUse";
 import TextScroll from "@/components/Home/TextScroll";
